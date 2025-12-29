6f5ec1ec41c31d2dfc832a9a7d9e5fa7975c725f
Carlos Dordelly <40637572+cdordelly@users.noreply.github.com>
2025-12-29 14:20:08 -0300

    version update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## [1.0.1] - 2025-12-29
+### Added
+- Blender's extensions platform feedback updates for initial release.
+
 ## [1.0.0] - 2025-12-18
 ### Added
 - Initial Release
--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "dy Pack Master",
     "author": "Carlos Dordelly - @cdordelly",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (4, 0, 0),
     "location": "View3D > UI > dy Pack Master",
     "description": "Make Blender project files portable for render farms by localizing assets, addons, and dependencies.",
